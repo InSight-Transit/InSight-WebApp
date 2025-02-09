d933dc14e6a5e279a0e2a8a21c773db61a83c0a0
GSTran <giovanniseverinotran@gmail.com>
2025-02-08 16:43:15 -0800

    Add window scaling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,30 @@ export default function Home() {
     <div className="bg-sky-700 min-h-screen w-full">
       <NavHeader/>
       <div className="flex flex-1 justify-center items-center">
-        <h1 className="text-white text-7xl font-bold p-14">InSight</h1>
+        <h1 className="text-white text-[8vw] font-bold p-[5vw]">InSight</h1>
       </div>
       <div className="flex flex-1 justify-center items-center">
-        <h2 className="text-white text-3xl font-bold pb-8">Choose Language</h2>
+        <h2 className="text-white text-[3vw] font-bold pb-[4vw]">Choose Language</h2>
       </div>
       <div className="flex flex-col items-center justify-center">
-        <div className="pb-10 flex flex-1 justify-center items-center gap-12 w-6/12"> 
-          <button className="border-none text-4xl w-6/12 h-24 outline-none bg-white text-black font-extrabold text-xl px-6 py-2 rounded-lg border-4 border-blue-800">
+        <div className="pb-[4vw] flex flex-1 justify-center items-center gap-[5vw] w-6/12"> 
+          <button className="border-none text-[4.0vw] w-1/2 h-[11vw] outline-none bg-white text-black font-semibold rounded-lg">
             English
           </button>
-          <button className="border-none text-4xl w-6/12 h-24 outline-none bg-white text-black font-extrabold text-xl px-6 py-2 rounded-lg border-4 border-blue-800">
+          <button className="border-none text-[4.0vw] w-1/2 h-[11vw] outline-none bg-white text-black font-semibold rounded-lg">
             Español
           </button>
         </div>
-        <div className="pb-10 flex flex-1 justify-center items-center gap-12 w-6/12"> 
-          <button className="border-none text-4xl w-6/12 h-24 outline-none bg-white text-black font-extrabold text-xl px-6 py-2 rounded-lg border-4 border-blue-800">
+        <div className="pb-[4vw] flex flex-1 justify-center items-center gap-[5vw] w-6/12"> 
+          <button className="border-none text-[4.0vw] w-1/2 h-[11vw] outline-none bg-white text-black font-semibold rounded-lg">
             中文
           </button>
-          <button className="border-none text-4xl w-6/12 h-24 outline-none bg-white text-black font-extrabold text-xl px-6 py-2 rounded-lg border-4 border-blue-800">
+          <button className="border-none text-[4.0vw] w-1/2 h-[11vw] outline-none bg-white text-black font-semibold rounded-lg">
             tiếng việt
           </button>
         </div>
-        <div className="pb-10 flex flex-1 justify-center items-center gap-12 w-6/12"> 
-          <button className="border-none text-4xl h-24 outline-none bg-white w-6/12 text-black font-extrabold text-xl px-6 py-2 rounded-lg border-4 border-blue-800">
+        <div className="pb-[4vw] flex flex-1 justify-center items-center gap-[5vw] w-6/12"> 
+          <button className="border-none text-[4.0vw] w-1/2 h-[11vw] outline-none bg-white text-black font-semibold rounded-lg">
             Tagalog
           </button>
         </div>
