4c717513079264500d5906aa1234b589585fb133
GSTran <giovanniseverinotran@gmail.com>
2025-02-20 16:56:55 -0800

    Add registration

---
+++ src/app/registration/page.tsx
@@ -0,0 +1,12 @@
+
+import NavHeader from "../header";
+
+export default function registration() {
+  return (
+    <div className="bg-sky-700 min-h-screen w-full">
+      <NavHeader/>
+      
+      
+    </div>
+  );
+}
